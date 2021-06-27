0e9d8f094b6302e753c92e125e82b7725ea69470
digitalRepresentation <kinemura@naver.com>
2021-06-28 08:04:24 +0900

    toggle anti pattern 解除

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -33,7 +33,7 @@ export default {
       this.todoItems.splice(index, 1);
     },
     toggleOneItem: function(todoItem, index) {
-      todoItem.completed = !todoItem.completed;
+      this.todoItems[index].completed = !this.todoItems[index].completed;
       localStorage.removeItem(todoItem.item);
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     }
