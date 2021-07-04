22a4cb82d7d0bf6a94ef4a6ce7009d3c81624111
digitalRepresentation <kinemura@naver.com>
2021-07-04 21:20:18 +0900

    modal import

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -4,10 +4,21 @@
       <span class="addCoutainer" v-on:click="addTodo">
         <i class="fas fa-plus addBtn"></i>
       </span>
+
+      
+      <modal v-if="showModal" @close="showModal = false">
+        <!--
+      you can use custom content here to overwrite
+      default content
+    -->
+        <h3 slot="header">custom header</h3>
+      </modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
 export default {
   data: function() {
     return {
@@ -27,6 +38,9 @@ export default {
     clearInput: function() {
       this.newTodoItem = '';
     }
+  },
+  components: {
+    Modal: Modal
   }
 }
 </script>
